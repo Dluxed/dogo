17343b75380a2e99168764820c0a4d3682e1440c
Dluxed <118645102+Dluxed@users.noreply.github.com>
2023-02-27 11:50:53 -0700

    Mas resolver errores

--- app/Traductor/page.jsx
+++ app/Traductor/page.jsx
@@ -1,38 +1,32 @@
 import React from "react";
 import "./page.css";
-import "./script";
+//import Codigito from "./script";
+
+
 export default function Traductor(){
   return (
-
-    <body>
-      <div class="conatiner">
+<body>
+    <div class="conatiner">
         <div>
-          <div class="header">
-            <select class="first">
-            </select>
-            <button class="iconButton" id="change"><i class="fas fa-exchange-alt"></i></button>
-            <select class="second"></select>
-          </div>
-          <div class="containerTextArea">
-            <div>
-              <textarea class="fromText" placeholder="Enter text"></textarea>
-              <div>
-                <button class="read iconButton"><i class="fas fa-volume-up"></i></button>
-                <button class="listen iconButton"><i class="fas fa-microphone"></i></button>
-              </div>
+            <div class="header">
+                <select class="first">
+                </select>
+                <select class="second"></select>
             </div>
-            <div>
-              <textarea class="toText" placeholder="Enter text"></textarea>
-              <div>
-                <button class="read iconButton"><i class="fas fa-volume-up"></i></button>
-              </div>
+            <div class="containerTextArea">
+                <div>
+                    <textarea class="fromText" placeholder="Enter text"></textarea>  
+                </div>
+                <div>
+                    <textarea class="toText" placeholder="Enter text"></textarea>
+                </div>
             </div>
-          </div>
-          <button class="translate">Translate</button>
+            <button class="translate">Translate</button>
         </div>
-      </div>
-      <script src="/script" type="module"></script>
-    </body>
-  );
+    </div>
+    <script src="./script.js" type="module"></script>
+</body>
+   );
 }
+
   
