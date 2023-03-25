4e4cf7b0ec7bb0adc95d92800fa131ed4507296d
Dluxed <118645102+Dluxed@users.noreply.github.com>
2023-03-24 18:27:50 -0700

    Cambios implementacion axios

--- app/Traductor/page.jsx
+++ app/Traductor/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
     Form,
     TextArea,
@@ -12,59 +12,89 @@ import axios from 'axios';//importar axios para el traductor
 
 //Funcion de traducir, se ejecutan las acciones necesarias para responder y realizar una traduccion
 export default function pageTranslator() {
-    //Valor inicial y valor a cambiar
-    const [inputText, setInputText] = useState("")
+    const [inputText, setInputText] = useState(""); //Valor inicial y valor a cambiar
+    const [detectLanguageKey, setdetectedLanguageKey] = useState(''); //Lenguaje a traducir, detecta que lenguaje hay en el primer textbox
+    const [selectedLanguageKey, setLanguageKey] = useState(''); //Ver key de lenguaje seleccionado en la lista
+    const [languagesList, setLanguagesList] = useState([]); //Lista de lenguajes
+    const [resultText, setResultText] = useState(''); //Texto resultado
+    //Obtener los lenguajes y utilizar la api axios para obtener idiomas, dar una respuesta de traduccion con los idiomas seleccionados.
+    const getLanguageSource = () => {
+         axios.post('https://libretranslate.de/detect', {
+             q: inputText
+         })
+             .then((response) => {
+                setdetectedLanguageKey(response.data[0].language)
+             })
+     }
+     useEffect(() => {
+         axios.get('https://libretranslate.de/languages')
+             .then((response) => {
+                 setLanguagesList(response.data)
+             })
+     getLanguageSource()}, [inputText])
 
-    //Crear evento para getLanguageSource
-    function getLanguageSource(e) {
-        e.preventDefault() //Previene opccion de seleccion automatica de componentes
-        console.log(inputText)
-    }
-
-    //Accion de onChange
-    function inputHandler(e) {
-        setInputText(e.target.value)
-    }
+     //Constante de leguagueKey
+     const languageKey = (selectedLanguage) => {
+         setLanguageKey(selectedLanguage.target.value)
+     }
+    
+    //Constante para seleccionar objetivos a traducir
+     const translateText = () => {
+         getLanguageSource();
+         let data = {
+             q : inputText,
+             source: detectLanguageKey,
+             target: selectedLanguageKey
+         }
+         axios.post(`https://libretranslate.de/translate`, data)
+         .then((response) => {
+             setResultText(response.data.translatedText)
+         })
+     }
 
     //valores a mostrar en la página
     return (
         <>
-            <div className="app-header">
-                <h2 className="header">Dogo Traductor</h2>
-            </div>
-
-            <div className='app-body'>
-                <div>
-                    {/*Un textarea donde se escribe el texto a traducir*/}
-                    <Form>
-                        <Form.Field
-                            control={TextArea}
-                            placeholder='Type Text to Translate..'
-                            onChange={inputHandler}
+            <div>
+             <div className="app-header">
+                 <h2 className="header">Dogo Traductor</h2>
+             </div> 
+             <div className='app-body'>
+                 <div>
+                     <Form>
+                         <Form.Field
+                             control={TextArea}
+                             placeholder='Escribe aquí el texto a traducir..'
+                             onChange={(e) => setInputText(e.target.value)}
                         />
-
-                        {/*Selecciona un lenguaje*/}
-                        <select className="language-select">
-                            <option>Please Select Language..</option>
-                        </select>
-
-                        {/*Otro textarea donde se muestra el resultado*/}
-                        <Form.Field
-                            control={TextArea}
-                            placeholder='Your Result Translation..'
-                        />
-
-                        {/*Boton para activar la funcion*/}
-                        <Button
-                            color="blue"
-                            size="large"
-                            onClick={getLanguageSource}
-                        >  {/*Traer icono con el nombre traducir de Semantic UI React*/}
-                            <Icon name='translate' />
-                            Translate</Button>
-                    </Form>
-                </div>
-            </div>
+                        
+                         <select className="language-select" 
+                         onChange={languageKey}>
+                             <option>Please Select Language..</option>
+                             {languagesList.map((language) => {
+                                 return (
+                                     <option value={language.code}>
+                                         {language.name}
+                                     </option>
+                                 )
+                             })}
+                         </select>
+                         <Form.Field
+                             control={TextArea}
+                             placeholder='Tu traduccción aparece aquí..'
+                             value={resultText}
+                         />
+                         <Button
+                             color="orange"
+                             size="large"
+                             onClick={translateText}
+                         >
+                             <Icon name='translate' />
+                             Translate</Button>
+                     </Form>
+                 </div>
+             </div>
+         </div>
 
         </>
 
